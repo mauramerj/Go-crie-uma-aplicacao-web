4515b6c7f80589e3b9d772a39f722a34f179f333
Maurício A. da Silva <mau.rame@hotmail.com>
2023-10-22 20:21:24 -0300

    Final do segundo capítulo - struct

--- main.go
+++ main.go
@@ -5,13 +5,26 @@ import (
 	"text/template"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	http.HandleFunc("/", Index)
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+func index(w http.ResponseWriter, r *http.Request) {
+	produtos := []Produto{
+		{"Camiseta", "Verde musgo", 2999, 20},
+		{"Bermuda", "Grená", 2599, 10},
+		{"Meias", "Vermelha tomate", 2000, 5},
+		{"Tênis", "Nike", 5999, 1},
+	}
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
