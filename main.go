ffbc4950353d86ce4ee9aaddc275de6a067932e3
Maurício A. da Silva <mau.rame@hotmail.com>
2023-10-22 19:55:00 -0300

    Página estilizada com Bootstrap

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", Index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "Index", nil)
+}
