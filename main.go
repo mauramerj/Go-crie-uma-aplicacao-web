8b3bd5a84cc64097170bddbbd476fd71e1ffbe3b
Maurício A. da Silva <mau.rame@hotmail.com>
2023-10-22 22:23:22 -0300

    Exibindo dados do banco (capitulo 02)

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"net/http"
 	"text/template"
+	//"github.com/lib/pq"
 )
 
 type Produto struct {
+	id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -15,16 +17,32 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := conectaComBancoDeDados()
+	defer db.Close()
+
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{"Camiseta", "Verde musgo", 2999, 20},
-		{"Bermuda", "Grená", 2599, 10},
-		{"Meias", "Vermelha tomate", 2000, 5},
-		{"Tênis", "Nike", 5999, 1},
+	db := conectaComBancoDeDados()
+
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
 	}
+
+	p := Produto{}
+	p := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+	}
+
 	temp.ExecuteTemplate(w, "Index", produtos)
 }
